076cbeaa53e062653e1e51e0d1961aad6ee31109
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-11-17 13:27:13 +0100

    Removed unused usings.

--- RepoCleanup/Functions/CreateRepoFunction.cs
+++ RepoCleanup/Functions/CreateRepoFunction.cs
@@ -1,10 +1,7 @@
 ﻿using RepoCleanup.Application.CommandHandlers;
 using RepoCleanup.Application.Commands;
-using RepoCleanup.Models;
 using RepoCleanup.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace RepoCleanup.Functions
