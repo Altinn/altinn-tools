ed3c5e4b40e2631c524f66b2c312311c89464009
Terje Holene <terje.holene@gmail.com>
2023-10-18 13:59:13 +0200

    Allow shortname to start with numbers

--- RepoCleanup/Functions/SharedFunctionSnippets.cs
+++ RepoCleanup/Functions/SharedFunctionSnippets.cs
@@ -133,7 +133,7 @@ namespace RepoCleanup.Functions
                 isValid = IsValidOrgShortName(shortName);
                 if (!isValid)
                 {
-                    Console.WriteLine("Invalid name. Letters a-z and character '-' are permitted. Username must start with a letter and end with a letter or number.");
+                    Console.WriteLine("Invalid name. The name must consist of letters or/and digits.");
                 }
             }
 
@@ -142,7 +142,7 @@ namespace RepoCleanup.Functions
 
         private static bool IsValidOrgShortName(string shortName)
         {
-            return Regex.IsMatch(shortName, "^[a-z]+[a-z0-9]$");
+            return Regex.IsMatch(shortName, "^[a-z0-9]+$");
         }
 
         public static void ConfirmWithExit(string confirmMessage, string exitMessage)
